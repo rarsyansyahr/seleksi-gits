f8a845607a258758bab54b247ef9af88f6dd74f6
rarsyansyahr <rarsyansyahr@gmail.com>
2021-07-29 04:40:58 +0700

    Inisialisasi Projek

---
+++ bin/seleksi_gits.dart
@@ -0,0 +1,5 @@
+import 'package:seleksi_gits/seleksi_gits.dart' as seleksi_gits;
+
+void main(List<String> arguments) {
+  print('Hello world: ${seleksi_gits.calculate()}!');
+}
---
+++ test/seleksi_gits_test.dart
@@ -0,0 +1,8 @@
+import 'package:seleksi_gits/seleksi_gits.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
